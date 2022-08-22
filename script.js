d67be3e3615b45cb04647974ff34953aaf339192
TackRegnar <TackRegnar@protonmail.com>
2022-08-22 01:01:21 -0500

    Password match checking complete

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//ToDo Do passwords match?
+function check_password() {
+  let p1 = document.getElementById("password");
+  let p2 = document.getElementById("password_confirm");
+  let pcl = document.getElementById("password_confirm_label")
+
+  if (p1.value === "" && p2.value === "") {
+    p1.style.backgroundColor = "#E5E7EB";
+    p2.style.backgroundColor = "#E5E7EB";
+    return;
+  }
+
+  if (p1.value != "" && p2.value === "") {
+    p1.style.backgroundColor = "green";
+    p2.style.backgroundColor = "#E5E7EB";
+    return;
+  }
+
+  if (p1.value != "" && p2.value != "") {
+
+    if (p1.value === p2.value) {
+      p1.style.backgroundColor = "green";
+      p2.style.backgroundColor = "green";
+      pcl.textContent = "Confirm Password"
+      return;
+    }
+
+    if (p1.value != p2.value) {
+      p1.style.backgroundColor = "green";
+      p2.style.backgroundColor = "red";
+      pcl.textContent = "Confirm Password *Password does not match*"
+      return;
+    }
+  }
+}
+
+check_password();
+
